b6fd3bad4e561d0e8ce618c1db7fb837250ccd7a
Sarath Chandra <132736634+sarath4746@users.noreply.github.com>
2025-01-12 21:09:08 +0530

    Create Rotten Oranges .java

---
+++ RottenOranges.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int freshCount = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 2) {
+                    queue.add(new int[]{r, c});
+                } else if (grid[r][c] == 1) {
+                    freshCount++;
+                }
+            }
+        }
+
+        if (freshCount == 0) return 0;
+
+        int minutes = 0;
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            boolean rotten = false;
+
+            for (int i = 0; i < size; i++) {
+                int[] cell = queue.poll();
+                int row = cell[0];
+                int col = cell[1];
+
+                for (int[] dir : directions) {
+                    int newRow = row + dir[0];
+                    int newCol = col + dir[1];
+
+                    if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols && grid[newRow][newCol] == 1) {
+                        grid[newRow][newCol] = 2;
+                        queue.add(new int[]{newRow, newCol});
+                        freshCount--;
+                        rotten = true;
+                    }
+                }
+            }
+
+            if (rotten) minutes++;
+        }
+
+        return freshCount == 0 ? minutes : -1;
+    }
+}
